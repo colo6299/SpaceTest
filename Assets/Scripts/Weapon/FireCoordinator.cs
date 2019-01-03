5bc7882c48da445427b3439a00bbce2a8befe976
Gauge <Gauger16@gmail.com>
2019-01-02 18:26:45 -0800

    Added sequencing

--- Assets/Scripts/Weapon/FireCoordinator.cs
+++ Assets/Scripts/Weapon/FireCoordinator.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FireCoordinator : MonoBehaviour {
+/// <summary>
+/// Handles weapons control from users
+/// 
+/// Switching primaries and such
+/// </summary>
+public class FireCoordinator : MonoBehaviour
+{
+    public bool IsFiring { get; private set; }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+    // The current selected weapon (assuming more than one type of weapon is)
+    public MonoBehaviour Selected;
+
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            IsFiring = true;
+        }
+        else
+        {
+
+        }
+
+
+    }
 }

678dfe22e7d07c9f5274f94af3c9af53b1c55a57
colo6299 <rocketblox@gmail.com>
2019-01-03 00:40:22 -0700

    HDR Skybox/Tracers

---
+++ Assets/Scripts/Enemies/DestroyableCoordinator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyableCoordinator : MonoBehaviour {
+
+
+    public float health = 100;
+    public GameObject dmgPrefab;
+
+    
+    public void TakeDamage(float dmg)
+    {
+        health -= dmg;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+            Destroy(Instantiate(dmgPrefab, transform.position, transform.rotation, null), 5f);
+        }
+    }
+
+
+
+
+
+
+
+}
---
+++ Assets/Scripts/Projectile/StandardProjectile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StandardProjectile : MonoBehaviour {
+
+    public float MaxTrajectory = 1000f;
+    public float Damage = 10f;
+    public float Speed = 50f;
+
+    private float CurrentTrajectory = 0;
+
+    // Update is called once per frame
+    void Update ()
+    {
+        float distance = Speed * Time.deltaTime;
+        CurrentTrajectory += distance;
+        transform.position += transform.forward * distance;
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, distance))
+        {
+            DestroyableCoordinator ship = hit.transform.GetComponent<DestroyableCoordinator>();
+
+            ship.TakeDamage(Damage);
+            Destroy(gameObject);
+        }
+        else if (CurrentTrajectory >= MaxTrajectory)
+        {
+            Destroy(gameObject);
+        }
+	}
+}

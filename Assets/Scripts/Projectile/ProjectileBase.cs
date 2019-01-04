bea453a14cfb3f1b61aebaad048c726a31bc5c78
colo6299 <rocketblox@gmail.com>
2019-01-03 22:43:12 -0700

    Merge branch 'master' into VisualsTest

---
+++ Assets/Scripts/Projectile/ProjectileBase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Projectile
+{
+    public abstract class ProjectileBase : MonoBehaviour
+    {
+        public Rigidbody Parent;
+
+        public float MaxTrajectory = 1000f;
+        public float Damage = 10f;
+        public float Speed = 50f;
+
+        private Vector3 inharetedVelocity;
+        private float currentTrajectory = 0;
+
+        private void Start()
+        {
+            inharetedVelocity = Parent.velocity;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float distance = Speed * Time.deltaTime;
+            currentTrajectory += distance;
+            transform.position += ((transform.forward * distance) + inharetedVelocity * Time.deltaTime);
+
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, distance))
+            {
+                DestroyableCoordinator ship = hit.transform.GetComponent<DestroyableCoordinator>();
+
+                ship.TakeDamage(Damage);
+                Destroy(gameObject);
+            }
+            else if (currentTrajectory >= MaxTrajectory)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+    }
+}
--- Assets/Scripts/Projectile/StandardProjectile.cs
+++ Assets/Scripts/Projectile/StandardProjectile.cs
@@ -1,39 +1,10 @@
-﻿using System.Collections;
+﻿using Assets.Scripts.Projectile;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StandardProjectile : MonoBehaviour {
-
-    public float MaxTrajectory = 1000f;
-    public float Damage = 10f;
-    public float Speed = 50f;
-
-    private float CurrentTrajectory = 0;
-
-    // Update is called once per frame
-    void Update ()
-    {
-        float distance = Speed * Time.deltaTime;
-        CurrentTrajectory += distance;
-        transform.position += transform.forward * distance;
-
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit, distance))
-        {
-
-            if (hit.transform.gameObject.GetComponent<DestroyableCoordinator>() != null)
-            {
-                DestroyableCoordinator ship = hit.transform.GetComponent<DestroyableCoordinator>();
-                ship.TakeDamage(Damage);              
-            }
-
-            Destroy(gameObject);
+public class StandardProjectile : ProjectileBase
+{
 
 
-        }
-        else if (CurrentTrajectory >= MaxTrajectory)
-        {
-            Destroy(gameObject);
-        }
-	}
 }

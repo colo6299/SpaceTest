993b33b41f615877ff57600e6084300f76602a5b
colo6299 <rocketblox@gmail.com>
2019-01-04 05:44:36 -0700

    Braindead (TM) AI Systems

--- Assets/Scripts/Enemies/AI/EnemyAI.cs
+++ Assets/Scripts/Enemies/AI/EnemyAI.cs
@@ -4,5 +4,140 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour {
 
-    
+
+    public GameObject player;
+
+    private EntityInfo info;
+    private float speed;
+
+    private string navState = "Standby";
+    private float distance;
+    private Rigidbody prb;
+    private Rigidbody rb;
+    private float minStrafe = 100f;
+    private float minStart = 400f;
+    private float maxStart = 800f;
+
+    private bool strafing = false;
+    private bool stalking = false;
+    private bool slewing = false;
+    private bool retreating = false;
+
+    private Vector3 playerPrediction;
+    private Vector3 targetPoint;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        prb = player.GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
+        info = GetComponent<EntityInfo>();
+        UpdateStats();
+    }
+
+
+    void Update()
+    {
+        targetPoint = playerPrediction;
+
+        NavChoice();
+
+        if (slewing) { MoveSlew(); }
+        else { MoveStandard(); }
+
+    }
+
+
+    public void UpdateStats()
+    {
+        speed = info.MaxSpeed;
+    }
+
+
+    void NavChoice()
+    {
+        distance = (player.transform.position - transform.position).magnitude;
+        playerPrediction = player.transform.position + prb.velocity;
+
+        if (distance < minStrafe)
+        {
+            strafing = false;
+        }
+
+        if (distance > minStart)
+        {
+            strafing = true;
+        }
+
+        if (strafing)
+        {
+            Strafe();
+        }
+
+        else if (distance < maxStart)
+        {
+            Retreat();
+        }
+
+        else
+        {
+            Stalk();
+        }
+    }
+
+
+    void FireControl()
+    {
+        if (strafing)
+        {
+            Fire();
+        }
+    }
+
+
+    void Fire()
+    {
+
+    }
+
+
+    void MoveStandard()
+    {
+        rb.velocity = transform.forward * speed;
+
+        Vector3 rotVec = transform.InverseTransformPoint(targetPoint);
+        rotVec = new Vector3(-rotVec.y, rotVec.x, 0);
+
+        Debug.Log(targetPoint);
+
+        rb.angularVelocity = transform.TransformDirection(rotVec.normalized);
+
+    }
+
+
+    void MoveSlew()
+    {
+
+    }
+
+
+    void Strafe()
+    {
+
+    }
+
+
+    void Stalk()
+    {
+
+    }
+
+
+    void Retreat()
+    {
+         
+    }
+
+
+
 }

c29fc9c4d9d7ebe233d56d2df327e4908c21ccb7
colo6299 <rocketblox@gmail.com>
2019-01-10 01:30:20 -0700

    BaseRoller Pro (TM)

--- Assets/Scripts/LoadManagment/PrefabHolder.cs
+++ Assets/Scripts/LoadManagment/PrefabHolder.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 public class PrefabHolder : MonoBehaviour {
 
+
+    /// <summary>
+    /// Misc. prefabs 
+    /// </summary>
     public GameObject[] prefabArray;
+
+    public GameObject[] itemArray;
+    public static GameObject[] staticItemArray;
+    public static int itemLength;
+
+    public GameObject[] bossArray;
+
+    void Start()
+    {
+        itemLength = itemArray.Length;
+        staticItemArray = itemArray;
+    }
 
 }

7a0ee444f28e8ed41ee185b31032e8d794eae025
colo6299 <rocketblox@gmail.com>
2019-01-11 14:56:28 -0700

    PFHolder Added Icon Support

--- Assets/Scripts/LoadManagment/PrefabHolder.cs
+++ Assets/Scripts/LoadManagment/PrefabHolder.cs
@@ -14,12 +14,15 @@ public class PrefabHolder : MonoBehaviour {
     public static GameObject[] staticItemArray;
     public static int itemLength;
 
+    public Texture2D[] iconArray;
+    public static Texture2D[] staticIconArray;
+
     public GameObject[] bossArray;
 
     void Start()
     {
         itemLength = itemArray.Length;
         staticItemArray = itemArray;
+        staticIconArray = iconArray;
     }
-
 }

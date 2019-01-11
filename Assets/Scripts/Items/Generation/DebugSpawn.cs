15a3ebb6710dc540c4efadcc27402c32977cf3b0
colo6299 <rocketblox@gmail.com>
2019-01-11 15:42:12 -0700

    Item Drop v1

---
+++ Assets/Scripts/Items/Generation/DebugSpawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugSpawn : MonoBehaviour {
+
+    public GameObject item;
+    private float sTime = 0;
+    private GameObject spawn;
+
+    void Update()
+    {/*
+        if (Time.time - sTime > 10 & spawn == null)
+        {
+            spawn = Instantiate(item);
+            spawn.GetComponent<PickupItem>().roll = BaseRoller.RollItem(100);
+        }*/
+
+
+    }
+    void Start()
+    {
+        spawn = Instantiate(item, transform.position, transform.rotation, null);
+        spawn.GetComponent<PickupItem>().roll = BaseRoller.RollItem(100);
+    }
+}
---
+++ Assets/Scripts/Items/PickupItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupItem : MonoBehaviour {
+
+    public RollInfo roll;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponentInParent<Rigidbody>().gameObject.tag == "Player")
+        {
+            //whaaaaaaat no way this is bunk... not at aaaaalllll
+            BaseRoller.InstantiateItem(roll, other.GetComponentInParent<Rigidbody>().transform.position, other.GetComponentInParent<Rigidbody>().transform.rotation, other.GetComponentInParent<Rigidbody>().transform.GetChild(2)).SetActive(false);
+            Destroy(gameObject);
+        }
+    }
+
+
+
+}
